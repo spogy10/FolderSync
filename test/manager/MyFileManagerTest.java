a5bb6fae3f5f846df7d21ddec08bfa78053c4078
poliverjr <poliverjr@hotmail.com>
2019-08-03 19:51:19 -0500

    added unit test for getItemsList

---
+++ test/manager/MyFileManagerTest.java
@@ -0,0 +1,29 @@
+package manager;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import utility.Settings;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyFileManagerTest {
+
+    MyFileManager myFileManager;
+
+    @BeforeEach
+    void init(){
+        myFileManager = MyFileManager.getInstance(Settings.getInstance().getValue(Settings.SettingsKeys.FOLDER_LOCATION));
+    }
+
+
+    @Test
+    void getItemsList() {
+        int expectedValue = 7;
+
+        List<String> fileList = myFileManager.getItemsList();
+
+        assertEquals(expectedValue, fileList.size());
+    }
+}
