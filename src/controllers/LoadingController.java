1d8296c1e0deda8530267861023b8e3846440d2a
poliverjr <poliverjr@hotmail.com>
2019-08-13 20:34:59 -0500

    added loading controller

---
+++ src/controllers/LoadingController.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoadingController implements Initializable {
+
+    public static final String TITLE = "Loading";
+    public static final String FXML = "/views/loading.fxml";
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
